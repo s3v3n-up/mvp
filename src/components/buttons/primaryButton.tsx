f42021a1044d75d6052b1ccb0e35193d8aebe88d
Ryan Urbien <dmncrynn@gmail.com>
2022-10-12 19:15:58 -0700

    created components

---
+++ src/components/buttons/primaryButton.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/styles/Buttons.module.sass";
+
+export default function PrimaryButton() {
+  return (
+    <>
+      <div className={styles.primarybutton}>
+        <button type="button" placeholder="button">
+          primary button
+        </button>
+      </div>
+    </>
+  );
+}
---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/navbar/Navbar";
+import style from "@/styles/Layout.module.sass";
+import React from "react"
+
+const Layout = ({ children }) => {
+    return (
+        
+        <div className={style.container}>
+            <Navbar/>
+            { children }
+        </div>
+    );
+};
+
+export default Layout;
