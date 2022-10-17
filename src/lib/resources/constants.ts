598ec64d446a908baec2073788452e7626face88
RazielGelo <angeloguerra1986x@gmail.com>
2022-10-16 22:10:36 -0700

    Applied ESlint

--- src/lib/resources/constants.ts
+++ src/lib/resources/constants.ts
@@ -1,5 +1,5 @@
-export const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/
+export const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/;
 
-export const PHONE_REGEX = /\d{3}-\d{3}-\d{4}/
+export const PHONE_REGEX = /\d{3}-\d{3}-\d{4}/;
 
 
--- src/lib/types/Match.ts
+++ src/lib/types/Match.ts
@@ -1,4 +1,4 @@
-import { ObjectId } from 'mongodb'
+import { ObjectId } from "mongodb";
 
 /**
  * Interface for creating a match
@@ -9,8 +9,8 @@ import { ObjectId } from 'mongodb'
  * @location This is the location where the match is or will happen
  * @matchStart This is the start date/time of the match
  * @matchEnd This is the end date/time of the match
- * @description This is the match details 
- * @teamA This is the home team 
+ * @description This is the match details
+ * @teamA This is the home team
  * @teamB This is the away team
  */
 
