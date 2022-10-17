b4202f48ce6b63852403c46ac94839ed14bdd5e7
RazielGelo <angeloguerra1986x@gmail.com>
2022-10-16 22:23:29 -0700

    Applied ESlint and removed unrelated folders

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,28 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from 'next/server'
+import type { NextRequest } from "next/server";
 
-import { getToken } from 'next-auth/jwt';
+import { getToken } from "next-auth/jwt";
 
 /**
- * @middleware 
+ * @middleware
  * This is where you can input logic for your middleware
  */
 
 export async function middleware(req: NextRequest) {
-	const secret = process.env.NEXTAUTH_SECRET;
+    const secret = process.env.NEXTAUTH_SECRET;
 
-	let token = await getToken(
-		{
-			req,
-			secret
-		}
-	);
-	console.log("token", token)
-	if (!token) {		
-		return NextResponse.rewrite(new URL('/login', req.url))		
-	}
-	return NextResponse.next();
+    let token = await getToken(
+        {
+            req,
+            secret
+        }
+    );
+    console.log("token", token);
+    if (!token) {
+        return NextResponse.rewrite(new URL("/login", req.url));
+    }
+
+    return NextResponse.next();
 }
 
 /**
@@ -30,5 +31,5 @@ export async function middleware(req: NextRequest) {
 
 
 export const config = {
-	matcher: ['/api/register']
-  }
+    matcher: ["/api/register"]
+};
