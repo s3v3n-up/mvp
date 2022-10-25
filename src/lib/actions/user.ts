c0359a20a0959a0f9ce5ffdfebbc26f015ad221d
emilsangil <sangilemiljan@gmail.com>
2022-10-24 20:23:21 -0700

    remove unnecessary space and also add spaces to separete imports to code

--- src/lib/actions/user.ts
+++ src/lib/actions/user.ts
@@ -1,9 +1,12 @@
 // Imports UserProfile interface
 import { UserProfile } from "@/lib/types/User";
+
 // Imports UserModel Schema
 import UserModel from "../resources/models/User";
+
 // import Database from  '@/lib/resources/database'
 import Database from "@/lib/resources/database";
+
 /**
  * @description a function that creates user and save to the database
  * @param user accepts a user object
@@ -11,7 +14,7 @@ import Database from "@/lib/resources/database";
  */
 export async function createUser(user: UserProfile) {
     try {
-        // Sets up the database
+    // Sets up the database
         await Database.setup();
         // Deconstruct user to get username and email
         const { userName, email } = user;
@@ -20,41 +23,42 @@ export async function createUser(user: UserProfile) {
         // Check if the email already exist in the database
         const existingEmail = await UserModel.findOne({ email });
         // If username exist returns an error code and message
-        if(existingUser || existingEmail) {
+        if (existingUser || existingEmail) {
             return {
                 code: 400,
-                message: "Username or Email already taken"
+                message: "Username or Email already taken",
             };
         }
         // Creates a UserModel
         const player = new UserModel<UserProfile>(user);
         // Saves the UserModel in the database
         await player.save();
-
         // Returns a code and message for successful creation of user
+
         return {
             code: 200,
-            message: "User successfully created"
+            message: "User successfully created",
         };
-        // Catch any errors and throws a message
-    } catch(error: any) {
+    // Catch any errors and throws a message
+    } catch (error: any) {
         throw new Error("Error creating a user", error.message);
     }
 }
+
 /**
  * @description A function that gets all users in the database and returns it
  */
 export async function getUsers() {
     try {
-        // Sets up the database
+    // Sets up the database
         await Database.setup();
         // Stores all the users in the database
         const users = await UserModel.find({});
 
         // Returns all users
         return users;
-        // Catches any error and throws it in message
-    } catch(error: any) {
+    // Catches any error and throws it in message
+    } catch (error: any) {
         throw new Error("Error getting users", error.message);
     }
 }
