41cbf93bef887998168417d63ce2f5b485391bf7
Son Minh Nguyen <000433430@student.vcc.ca>
2022-10-26 19:48:27 -0700

    fix some styling

--- src/pages/scoreboard.tsx
+++ src/pages/scoreboard.tsx
@@ -10,15 +10,24 @@ export default function Scoreboard(){
                     alt="crown icon"
                     layout="fill"
                     objectFit="contain"
-                    objectPosition={"center"}
+                    objectPosition="center"
                 />
             </div>
             <div className={styles.scoreboard}>
                 <div className={styles.hometeam}>
                     <h1 className={styles.home}>Home</h1>
-                    <div className={styles.scoreh}>
-                        <div className={styles.points}>
-                            <h1>24</h1>
+                    <div className="relative w-full h-40 flex flex-col justify-center">
+                        <div className="absolute bg-white w-full h-full rounded">
+                            <Image
+                                src="/scorebg.svg"
+                                layout="fill"
+                                objectFit="cover"
+                                objectPosition="center"
+                                alt="scorebg"
+                            />
+                        </div>
+                        <div className="z-10 bg-white p-5 w-11/12 m-auto rounded-md border-2 border-orange-600">
+                            <h1 className="text-black font-extrabold text-3xl text-center">8</h1>
                         </div>
                     </div>
                     <div className={styles.homeplayers}>
@@ -38,15 +47,22 @@ export default function Scoreboard(){
                             <button className={styles.homeleave}> Leave </button>
                         </div>
                     </div>
-                    <div className={styles.button}>
-                        <button className={styles.pause}> Pause </button>
-                    </div>
+                    <button className={styles.pause}> Pause </button>
                 </div>
                 <div className={styles.awayteam}>
                     <h1 className={styles.away}>Away</h1>
-                    <div className={styles.scorea}>
-                        <div className={styles.points}>
-                            <h1>8</h1>
+                    <div className="relative w-full h-40 flex flex-col justify-center">
+                        <div className="absolute bg-orange-600 w-full h-full rounded">
+                            <Image
+                                src="/scorebg.svg"
+                                layout="fill"
+                                objectFit="cover"
+                                objectPosition="center"
+                                alt="bg"
+                            />
+                        </div>
+                        <div className="z-10 bg-white p-5 w-11/12 m-auto rounded-md">
+                            <h1 className="text-black font-extrabold text-3xl text-center">8</h1>
                         </div>
                     </div>
                     <div className={styles.awayplayers}>
@@ -66,9 +82,7 @@ export default function Scoreboard(){
                             <button className={styles.leave}> Leave </button>
                         </div>
                     </div>
-                    <div className={styles.buttone}>
-                        <button className={styles.finish}> Finish </button>
-                    </div>
+                    <button className={styles.finish}> Finish </button>
                 </div>
             </div>
         </div>
