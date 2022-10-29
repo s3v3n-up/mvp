d96cd7da3d0e7cbf356ca034a90daeff1c6deffc
emilsangil <sangilemiljan@gmail.com>
2022-10-28 20:14:56 -0700

    Added comments

--- src/seed.ts
+++ src/seed.ts
@@ -1,10 +1,15 @@
+// Imports data from Data.json
 import data from "@/lib/resources/data/data.json";
 
+// Imports Sports Schema
 import Sport from "@/lib/resources/models/Sport";
 
+// Function that creates many sports model in the database
 export default async function seedAll() {
     return Promise.all(
         [
+
+            // Creates many sports model in the database
             Sport.insertMany(data)
         ]
     );
