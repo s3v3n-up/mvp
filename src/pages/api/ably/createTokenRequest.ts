9dd5a2542eb6c24ccb68b29426cc2faf3ab8d26d
Son Minh Nguyen <000433430@student.vcc.ca>
2022-10-30 19:33:05 -0700

    add comment to useChannel hook

--- src/pages/api/ably/createTokenRequest.ts
+++ src/pages/api/ably/createTokenRequest.ts
@@ -2,6 +2,9 @@ import Ably from "ably/promises";
 import { NextApiRequest, NextApiResponse } from "next";
 import { env } from "process";
 
+/**
+ * create token request for ably
+ */
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const client = new Ably.Realtime(env.ABLY_API_KEY!);
     const tokenRequestData = await client.auth.createTokenRequest({ clientId: "mvp_clientId" });
