0b4313c32272f70ceba25c1cc123f40a280a913a
Ryan Urbien <dmncrynn@gmail.com>
2022-10-30 19:35:17 -0700

    minor changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,45 +14,45 @@ const names = ["whale", "squid", "turtle", "coral", "starfish", "star"];
  *
  */
 export default function Home() {
-    return (
-        <Layout>
-            <div className={styles.matches}>
-                <div className={styles.search}>
-                    <h1>Matches</h1>
-                    <div className={styles.searchitem}>
-                        <input type={"text"} placeholder={"Enter username or location"} />
-                        <button>
-                            <Image
-                                src={"/icons/search.png"}
-                                width={17}
-                                height={17}
-                                alt={"logo"}
-                            ></Image>
-                        </button>
-                    </div>
-                </div>
+  return (
+    <Layout>
+      <div className={styles.matches}>
+        <div className={styles.search}>
+          <h1>Matches</h1>
+          <div className={styles.searchitem}>
+            <input type={"text"} placeholder={"Enter username or location"} />
+            <button>
+              <Image
+                src={"/icons/search.png"}
+                width={17}
+                height={17}
+                alt={"logo"}
+              ></Image>
+            </button>
+          </div>
+        </div>
 
-                <div>
-                    <p>Quick Matches</p>
-                    <div className={styles.container}>
-                        {names.map((name) => (
-                            <div className={styles.cards} key={name}>
-                                {name}
-                            </div>
-                        ))}
-                    </div>
-                </div>
-                <div>
-                    <p>Regular Matches</p>
-                    <div className={styles.container}>
-                        {names.map((name) => (
-                            <div className={styles.cards} key={name}>
-                                {name}
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </Layout>
-    );
+        <div>
+          <p>Quick Matches</p>
+          <div className={styles.container}>
+            {names.map((name) => (
+              <div className={styles.cards} key={name}>
+                {name}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div>
+          <p>Regular Matches</p>
+          <div className={styles.container}>
+            {names.map((name) => (
+              <div className={styles.cards} key={name}>
+                {name}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
