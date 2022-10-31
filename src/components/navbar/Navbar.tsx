4836870328f47f1aaba2ccaaa938f3ac1fb429d3
Ryan Urbien <dmncrynn@gmail.com>
2022-10-30 19:38:02 -0700

    minor changes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -11,53 +11,53 @@ import styles from "@/styles/Components.module.sass";
  */
 
 export default function Navbar() {
-  return (
-    <>
-      <div className={styles.nav}>
-        <Image
-          className={styles.img}
-          src={"/icons/logo.png"}
-          alt="logo"
-          width={130}
-          height={100}
-        />
-        <div className={styles.option}>
-          <Link href={"/"}>
-            <p>Ranking</p>
-          </Link>
-          <Link href={"/"}>
-            <p>Matches</p>
-          </Link>
-          <Link href={"/"}>
-            <p>Create Match</p>
-          </Link>
-          <Link href={"/"}>
-            <p>History</p>
-          </Link>
-        </div>
-        <div className={styles.auth}>
-          <Image src={"/icons/avatar.png"} alt="logo" width={25} height={25} />
-          <Link href={"/"}>
-            <p>Logout</p>
-          </Link>
-        </div>
-      </div>
-      <div className={styles.bottomNav}>
-        <div className={styles.option}>
-          <Link href={"/"}>
-            <p>Ranking</p>
-          </Link>
-          <Link href={"/"}>
-            <p>Matches</p>
-          </Link>
-          <Link href={"/"}>
-            <p>Create Match</p>
-          </Link>
-          <Link href={"/"}>
-            <p>History</p>
-          </Link>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={styles.nav}>
+                <Image
+                    className={styles.img}
+                    src={"/icons/logo.png"}
+                    alt="logo"
+                    width={130}
+                    height={100}
+                />
+                <div className={styles.option}>
+                    <Link href={"/"}>
+                        <p>Ranking</p>
+                    </Link>
+                    <Link href={"/"}>
+                        <p>Matches</p>
+                    </Link>
+                    <Link href={"/"}>
+                        <p>Create Match</p>
+                    </Link>
+                    <Link href={"/"}>
+                        <p>History</p>
+                    </Link>
+                </div>
+                <div className={styles.auth}>
+                    <Image src={"/icons/avatar.png"} alt="logo" width={25} height={25} />
+                    <Link href={"/"}>
+                        <p>Logout</p>
+                    </Link>
+                </div>
+            </div>
+            <div className={styles.bottomNav}>
+                <div className={styles.option}>
+                    <Link href={"/"}>
+                        <p>Ranking</p>
+                    </Link>
+                    <Link href={"/"}>
+                        <p>Matches</p>
+                    </Link>
+                    <Link href={"/"}>
+                        <p>Create Match</p>
+                    </Link>
+                    <Link href={"/"}>
+                        <p>History</p>
+                    </Link>
+                </div>
+            </div>
+        </>
+    );
 }
