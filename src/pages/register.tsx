79f3a17e70abccc382d96fef7dd8fabd0c5d6cb2
Son Minh Nguyen <000433430@student.vcc.ca>
2022-11-01 15:17:57 -0700

    add next-auth error handling for login

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -126,12 +126,6 @@ export default function Register() {
                 image: imageUrl,
                 matches: []
             });
-            const { ok, error } = res.data;
-            if (!ok) {
-                throw error;
-            } else {
-                router.push("/");
-            }
         } catch(err: any) {
             if (err!.response) {
                 setError(err.response.data.message);
