18cbe3ec090a0d9da7cc1a31f3d21e60e8409f14
Ryan Urbien <dmncrynn@gmail.com>
2022-11-03 22:48:16 -0700

    created a function and api route for getting all sports

--- src/components/tab/tab.tsx
+++ src/components/tab/tab.tsx
@@ -13,58 +13,61 @@ const hylians = ["link", "impa", "zelda"];
 
 //Tab function to render tabs and the container
 export default function Tab() {
-  //constants to indicate whether tab is active or not
-  const [activeTabIndex, setActiveTabIndex] = useState(0);
 
-  //Labels and content to populate the selected tab
-  const tabsData = [
-    {
-      label: "Created Matches",
-      content: (
-        <>
-          {names.map((name) => (
-            <Card startTime={""} sport={""} location={""} key={name} />
-          ))}
-        </>
-      ),
-    },
-    {
-      label: "History",
-      content: (
-        <>
-          {hylians.map((hylian) => (
-            <Card startTime={""} sport={""} location={""} key={hylian} />
-          ))}
-        </>
-      ),
-    },
-  ];
+    //constants to indicate whether tab is active or not
+    const [activeTabIndex, setActiveTabIndex] = useState(0);
 
-  //The rendered content
-  return (
-    <div className={styles.background}>
-      <div className={styles.titleContainer}>
-        {tabsData.map((tab, idx) => {
-          return (
-            //Tabs styling changes depending on it being clicked
-            <button
-              key={idx}
-              className={` ${
-                idx === activeTabIndex
-                  ? styles.selectedTab
-                  : styles.unselectedTab
-              }`}
-              //Change the active tab on click.
-              onClick={() => setActiveTabIndex(idx)}
-            >
-              {tab.label}
-            </button>
-          );
-        })}
-      </div>
-      <div className={styles.tabContent}>
-        <p>{tabsData[activeTabIndex].content}</p>
-      </div>
-    </div>
-  );
+    //Labels and content to populate the selected tab
+    const tabsData = [
+        {
+            label: "Created Matches",
+            content: (
+                <>
+                    {names.map((name) => (
+                        <Card startTime={""} sport={""} location={""} key={name} />
+                    ))}
+                </>
+            ),
+        },
+        {
+            label: "History",
+            content: (
+                <>
+                    {hylians.map((hylian) => (
+                        <Card startTime={""} sport={""} location={""} key={hylian} />
+                    ))}
+                </>
+            ),
+        },
+    ];
+
+    //The rendered content
+    return (
+        <div className={styles.background}>
+            <div className={styles.titleContainer}>
+                {tabsData.map((tab, idx) => {
+                    return (
+
+                    //Tabs styling changes depending on it being clicked
+                        <button
+                            key={idx}
+                            className={` ${
+                                idx === activeTabIndex
+                                    ? styles.selectedTab
+                                    : styles.unselectedTab
+                            }`}
+
+                            //Change the active tab on click.
+                            onClick={() => setActiveTabIndex(idx)}
+                        >
+                            {tab.label}
+                        </button>
+                    );
+                })}
+            </div>
+            <div className={styles.tabContent}>
+                <p>{tabsData[activeTabIndex].content}</p>
+            </div>
+        </div>
+    );
 }
