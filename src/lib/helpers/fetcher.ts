33107159eb6d24d1c3d53004d8490d9427804981
Son Minh Nguyen <000433430@student.vcc.ca>
2022-11-03 21:42:45 -0700

    add commit

--- src/lib/helpers/fetcher.ts
+++ src/lib/helpers/fetcher.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+/**
+ * fetcher function for swr hook
+ * @param url the url to fetch
+ * @returns data from the fetch
+ */
 export default async function fetcher(url: string) {
     const response = await axios.get(url);
 
