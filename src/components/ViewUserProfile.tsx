8f0c7e11ab9998d7bb1773bc0a4c288a992c5763
emilsangil <sangilemiljan@gmail.com>
2022-11-03 23:28:13 -0700

    added match view ui

--- src/components/ViewUserProfile.tsx
+++ src/components/ViewUserProfile.tsx
@@ -23,45 +23,45 @@ export interface Data {
  * @return display the other user's profile:fullname, username, phone, avatar, stats
  */
 export default function ViewUserProfile({ data }: { data: Data }) {
-  return (
-    <div className="flex justify-evenly">
-      <div className="flex w-1/4 flex-col space-y-3 lg:justify-end mt-10">
-        <div className="relative w-40 h-40 rounded-full m-auto mb-5">
-          <Image
-            src={data.image}
-            layout="fill"
-            objectFit="cover"
-            objectPosition="center"
-            alt="avatar"
-            className="rounded-full"
-          />
+    return (
+        <div className="flex justify-evenly">
+            <div className="flex w-1/4 flex-col space-y-3 lg:justify-end mt-10">
+                <div className="relative w-40 h-40 rounded-full m-auto mb-5">
+                    <Image
+                        src={data.image}
+                        layout="fill"
+                        objectFit="cover"
+                        objectPosition="center"
+                        alt="avatar"
+                        className="rounded-full"
+                    />
+                </div>
+                <Input label="Full Name" value={data.fullName} name="fullname" readonly>
+                    <Person />
+                </Input>
+                <Input label="User Name" value={data.userName} name="username" readonly>
+                    <FolderSharedOutlined />
+                </Input>
+                <Input label="Phone" value={data.phone} name="phone" readonly>
+                    <Phone />
+                </Input>
+                <table className=" border-collapse border border-slate-800 rounded-ml text-center mt-6 ">
+                    <thead className=" bg-[#fc5c3e] text-[#f3f2ef] my-5 py-5 mt-6">
+                        <tr className="my-5 py-5 mt-6">
+                            <th>Win</th>
+                            <th>Draw</th>
+                            <th>Loss</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className=" bg-white text-black">
+                            <td>{data.stats.win}</td>
+                            <td>{data.stats.draw}</td>
+                            <td>{data.stats.lose}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <Input label="Full Name" value={data.fullName} name="fullname" readonly>
-          <Person />
-        </Input>
-        <Input label="User Name" value={data.userName} name="username" readonly>
-          <FolderSharedOutlined />
-        </Input>
-        <Input label="Phone" value={data.phone} name="phone" readonly>
-          <Phone />
-        </Input>
-        <table className=" border-collapse border border-slate-800 rounded-ml text-center mt-6 ">
-          <thead className=" bg-[#fc5c3e] text-[#f3f2ef] my-5 py-5 mt-6">
-            <tr className="my-5 py-5 mt-6">
-              <th>Win</th>
-              <th>Draw</th>
-              <th>Loss</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className=" bg-white text-black">
-              <td>{data.stats.win}</td>
-              <td>{data.stats.draw}</td>
-              <td>{data.stats.lose}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+    );
 }
