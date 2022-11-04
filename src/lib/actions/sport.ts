3f9a862b0c901a56525f5abf120150a24e8d1e51
Ryan Urbien <dmncrynn@gmail.com>
2022-11-03 22:46:19 -0700

    created a function and api route for getting all sports

---
+++ src/lib/actions/sport.ts
@@ -0,0 +1,26 @@
+//local-import
+import SportModel from "../resources/models/Sport";
+import Database from "../resources/database";
+
+/**
+ * @description this is a function that gets all sports from the sport models
+ */
+
+export async function getAllSports() {
+    try {
+
+        //sets up database connection
+        await Database.setup();
+
+        //queries all sports and store to existing sports
+        const existingSports = await SportModel.find({});
+
+        //returns all existing sports
+        return existingSports;
+
+        //catches and throws an error
+    } catch(error: any) {
+        throw new Error("Error retrieving sports");
+    }
+
+}
---
+++ src/pages/api/sport/all.ts
@@ -0,0 +1,37 @@
+//third-part imports
+import { NextApiRequest, NextApiResponse } from "next";
+
+//local imports
+import { getAllSports } from "@/lib/actions/sport";
+
+/**
+ * @description this a function that handles api request for getting all sports
+ */
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    // If the HTTP method is GET
+    if (req.method === "GET") {
+        try {
+
+            //call the getAllSports function
+            const response = await getAllSports();
+
+            //returns the response as json
+            res.status(200).json({
+                response
+            });
+
+            //catches and throws a status code and error message
+        } catch (error: any) {
+
+            const { code = 500, message } = error;
+
+            res.status(code).json({
+                message
+            });
+
+            return;
+        }
+    }
+}
