eb0f95fd58362b5a4660327101549053d245692b
Abbe Azale <000394548@student.vcc.ca>
2022-11-06 00:01:17 -0700

    change file name

--- src/pages/api/ably/queueTimer.ts
+++ src/pages/api/ably/queueTimer.ts
@@ -1,9 +1,14 @@
 import Ably from "ably/promises";
 import { NextApiRequest, NextApiResponse } from "next";
 
+
+//game queue timer for when a player is waiting for a game to start
 export default async function gameQueue(req: NextApiRequest, res: NextApiResponse) {
     const client = new Ably.Realtime(process.env.ABLY_API_KEY!);
     const channel = client.channels.get("gameTimer");
     channel.publish("gameTimer", 30);
     res.status(200).json({ message: 30 });
+    
+
+
 }
