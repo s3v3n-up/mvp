1800603145b67a83ec26a250d2953ade21da2925
Yifei Chen <melodychen823@gmail.com>
2022-11-06 22:28:42 -0800

    add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+
+
+export default function NotFoundPage(){
+    return (
+        <div className="text-[#fc5c3e] text-center mt-32">
+            <h1 className="text-3xl my-10">Ooops... Sorry!</h1>
+            <h2 className="text-2xl">The page you are looking for that does not exist.</h2>
+        </div>
+    );
+}
