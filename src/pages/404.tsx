b0b01bd9223394038addbed5aa9ded65465eba82
Yifei Chen <melodychen823@gmail.com>
2022-11-08 02:33:32 -0800

    styling the 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,12 +5,16 @@ import Link from "next/link";
  */
 export default function NotFoundPage(){
     return (
-        <div className="text-[#fc5c3e] text-center mt-32">
-            <h1 className="text-3xl my-10">Ooops... Sorry!</h1>
-            <h2 className="text-2xl">The page you are looking for that does not exist.</h2>
-            <Link href="/">
-                <button className="bg-[#fc5c3e] text-white text-lg  rounded-lg py-2 px-3 my-10" >Back to Home</button>
-            </Link>
+        <div className="text-white text-center mt-32 flex-col">
+            <div className=" bg-[#fc5c3e]/60 rounded-xl mx-56 py-8">
+                <h1 className="text-3xl my-10">Ooops... Sorry!</h1>
+                <h2 className="text-2xl">The page you are looking for that does not exist.</h2>
+            </div>
+            <div>
+                <Link href="/">
+                    <button className="bg-[#fc5c3e] text-white text-lg  rounded-lg py-2 px-3 my-16" >Back to Home</button>
+                </Link>
+            </div>
         </div>
     );
 }
