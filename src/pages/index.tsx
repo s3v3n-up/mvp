24671af4f2ee7031e44f15e3635f61eed625f0e7
RazielGelo <angeloguerra1986x@gmail.com>
2022-11-07 16:29:38 -0800

    updated match creation to adjust for the adjustments made in  schema to lessen the query in mapbox, also updated index view to display location and kilometers from user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 //third-party import
-import Image from "next/image";
 import { useState, ChangeEvent, useEffect } from "react";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
+import haversine from "haversine-distance";
 
 //local import
 import styles from "@/styles/Home.module.sass";
@@ -12,6 +12,7 @@ import Cardstyles from "@/styles/MatchCard.module.sass";
 import Input from "@/components/Input";
 import { getMatches } from "@/lib/actions/match";
 import { getUsers } from "@/lib/actions/user";
+import { Location } from "@/lib/types/General";
 
 //dynamic import
 const Search = dynamic(() => import("@mui/icons-material/Search"), {
@@ -38,6 +39,41 @@ export default function Home({ regMatches, quickMatches, users }: any) {
 
     const [search, setSearch] = useState("");
 
+	 // Location useState
+	 const [currentLocation, setCurrentLocation] = useState<Location>();
+
+	 // Address useState
+	 const [address, setAddress] = useState<Location>();
+
+	 // useEffect to get user current location then set location to be saved in database
+	 useEffect(() => {
+
+        // options parameter for currentPosition function
+        const options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        };
+
+        // Success parameter for currentPosition function
+        const success = (pos: any) => {
+
+            // access position coordinates
+            const crd = pos.coords;
+
+            setCurrentLocation({
+                lat: crd.latitude,
+                lng: crd.longitude,
+            });
+        };
+
+        // Error parameter for currentPosition function
+        function error(err: any) {
+            console.warn(`ERROR(${err.code}): ${err.message}`);
+        }
+        navigator.geolocation.getCurrentPosition(success, error, options);
+    }, []);
+
     /**
    * handle search input change
    */
@@ -50,12 +86,6 @@ export default function Home({ regMatches, quickMatches, users }: any) {
    */
     function cardClicked(id: string) {
         return router.push(`/match/${id}/view`);
-    }
-
-    function hostAvatar(id: string) {
-        const host = users.filter((user: any) => user._id === id);
-
-        return host[0].image;
     }
 
     function lookUser(id: string) {
@@ -94,6 +124,7 @@ export default function Home({ regMatches, quickMatches, users }: any) {
                     </p>
                 )}
                 <ScrollContainer className="flex w-full" horizontal hideScrollbars>
+                    {/* //add comments */}
                     {quickMatches.length > 0 &&
             quickMatches
                 .filter(
@@ -121,14 +152,8 @@ export default function Home({ regMatches, quickMatches, users }: any) {
                             <div>
                                 <LocationOnIcon />
                             </div>
-                            <p>location</p>
-                            <Image
-                                src={hostAvatar(quick.matchHost)}
-                                alt="avatar"
-                                className={Cardstyles.avatar}
-                                width={45}
-                                height={45}
-                            />
+                            <p>{quick.location.address.pointOfInterest}</p>
+                            <p>{(haversine({ latitude: currentLocation?.lat as number, longitude: currentLocation?.lng as number },{ latitude: quick.location.lat as number, longitude: quick.location.lng as number }) / 1000).toFixed(2)}km away</p>
                         </div>
                     </div>
                 ))}
@@ -188,14 +213,8 @@ export default function Home({ regMatches, quickMatches, users }: any) {
                             <div>
                                 <LocationOnIcon />
                             </div>
-                            <p> location</p>
-                            <Image
-                                src={hostAvatar(reg.matchHost)}
-                                alt="avatar"
-                                className={Cardstyles.avatar}
-                                width={45}
-                                height={45}
-                            />
+                            <p>{reg.location.address.pointOfInterest}</p>
+                            <p>{(haversine({ latitude: currentLocation?.lat as number, longitude: currentLocation?.lng as number },{ latitude: reg.location.lat as number, longitude: reg.location.lng as number }) / 1000).toFixed(2)}km away</p>
                         </div>
                     </div>
                 ))}
