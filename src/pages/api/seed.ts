5463459dd6cfe1aed4dbfc37325b8c9c96e8c008
Emil Jan Sangil <000451738@student.vcc.ca>
2022-11-08 18:21:18 -0800

    Deployment

--- src/pages/api/seed.ts
+++ src/pages/api/seed.ts
@@ -22,7 +22,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return;
     }
 
-    // Sets up the database
+    // Sets up the database connection
     await Database.setup();
 
     // Await the function to be executed
